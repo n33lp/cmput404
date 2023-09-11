5bd3525ab414cd8a285f39edfe6b582ccf57d1e6
Neel <nsp@ualberta.ca>
2023-09-11 17:44:10 -0600

    lab 1 step 11

--- test1.py
+++ test1.py
@@ -1,3 +1,6 @@
 import requests
 
-print(requests.__version__)
+# print(requests.__version__)
+url = "https://raw.githubusercontent.com/n33lp/cmput404/master/test1.py"
+res = requests.get(url)
+print(res.text)
